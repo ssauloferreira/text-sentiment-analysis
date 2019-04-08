ecc59d862a9659b6a868debfcb71c82cd221459a
Saulo Ferreira <saulolucas.gm@gmail.com>
2019-04-08 11:50:06 -0300

    Flair embedding functions

--- Model_2/classes.py
+++ Model_2/classes.py
@@ -1,3 +1,6 @@
+from scipy.spatial import distance
+
+
 class Dataset:
     def __init__(self):
         self.docs = []
@@ -19,6 +22,7 @@ class Dataset:
 
         return new_docs
 
+
 class Data:
     def __init__(self, doc, label):
         self.doc = doc
@@ -29,3 +33,45 @@ class Data:
 
     def tostring(self):
         return self.label, self.doc
+
+
+class Flair_Embedding:
+    def __init__(self):
+        self.vocabulary = {}
+        flair_forward = FlairEmbeddings('news-forward-fast')
+        flair_backward = FlairEmbeddings('news-backward-fast')
+
+        self.stacked_embeddings = StackedEmbeddings(embeddings=[
+            flair_forward,
+            flair_backward
+        ])
+
+    def gen_vocabulary(self, vocabulary):
+        for word in vocabulary:
+            self.vocabulary[word] = self.stacked_embeddings.embed(word)
+
+    def embed_unique(self, word):
+        return self.stacked_embeddings.embed(word)
+
+    def similarity(self, word_a, word_b):
+        if self.vocabulary.__len__() == 0:
+            raise Exception('vocabulary is empty.')
+        else:
+            similarity = 1 / distance.euclidean(self.embedding(word_a), self.embedding(word_b))
+
+        return similarity
+
+    def most_similar(self, word, dictionary, num_of_similar):
+        most_similar = []
+        for word_b in dictionary:
+            most_similar.append([word_b, self.similarity(word, word_b)])
+
+        most_similar = sorted(most_similar, key=lambda x: x[1], reverse=True)
+
+        return most_similar[:num_of_similar]
+
+    def embedding(self, word):
+        if word not in self.vocabulary:
+            raise Exception('word is not in vocabulary.')
+        else:
+            return self.vocabulary[word]
