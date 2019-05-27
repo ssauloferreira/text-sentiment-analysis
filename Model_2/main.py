ee06115205a0028757f148477702eef6952e62e9
Saulo Ferreira <saulolucas.gm@gmail.com>
2019-05-27 13:27:59 -0300

    update

--- Model_2/main.py
+++ Model_2/main.py
@@ -22,7 +22,7 @@ def to_string(lists):
 
 
 nfeature = 8000
-n = 100
+n = 200
 src = 'books'
 tgt = 'electronics'
 
@@ -44,6 +44,7 @@ vocabulary_target = get_vocabulary(data_target)
 vocab_target, scores_target = get_senti_representation(vocabulary_target, True)
 
 # ----------------------------------- clustering ---------------------------------------------
+print("Clustering...")
 # clustering = DBSCAN(eps=1, min_samples=2)
 # clustering = SpectralClustering(n_clusters=n, assign_labels="discretize", random_state=0)
 clustering_source = KMeans(n_clusters=n, random_state=0)
@@ -70,6 +71,7 @@ for i in range(len(vocab_target)):
 #    print(text)
 
 # --------------------------------- feature selection ---------------------------------------
+print("Feature selection...")
 cv_source = CountVectorizer(max_df=0.95, min_df=2, max_features=10000)
 x_source = cv_source.fit_transform(to_string(data_source))
 
@@ -92,6 +94,7 @@ for feature in features_source:
 
 features_source = aux
 # --------------------------------- agrupando clusters --------------------------------------
+print("Linking clusters")
 grouped_s = []
 grouped_t = []
 
@@ -104,8 +107,6 @@ for i in range(n):
 
     index = 0
     sim = 0
-
-    #print(aux_features)
 
     for j in range(n):
         if j not in grouped_t:
@@ -169,6 +170,81 @@ for n in range(len(grouped_s)):
 
     print('\n\n')
 
-#for i in range(len(grouped_s)):
-#
+# ALSENT (Average-Lexical-SentiWordNet-TFIDF):
+# Average TF score = average of the term frequence in the documents it occurs
 # sentiment score = pos_score - (neg_score * -1)
+# ALSENT = Average TF score * sentiment score * IDF
+
+# ------------------------------ calculating ALSENT -------------------------------------
+print("ALSENT...")
+
+
+def get_average_tfidf(feature, data_source):
+    total = []
+    count_idf = 0
+    for text in data_source:
+        count = 0
+        for word in text:
+            if word == feature:
+                count += 1
+        if count > 0:
+            count_idf += 1
+            total.append(count)
+    return np.mean(total), np.log(len(data_source) / count_idf)
+
+
+sentcluster_source = []
+for i in range(len(wclusters_source)):
+    aux = []
+    for j in range(len(wclusters_source[i])):
+        avg_tf, idf = get_average_tfidf(wclusters_source[i][j], data_source)
+        sent = sclusters_source[i][j]
+        sent_value = sent[0] + (-1 * sent[1])
+        alsent = avg_tf * sent_value * idf
+
+        aux.append(alsent)
+
+    sentcluster_source.append(aux)
+
+sentcluster_target = []
+for i in range(len(wclusters_target)):
+    aux = []
+    for j in range(len(wclusters_target[i])):
+        avg_tf, idf = get_average_tfidf(wclusters_target[i][j], data_target)
+        sent = sclusters_target[i][j]
+        sent_value = sent[0] + (-1 * sent[1])
+        alsent = avg_tf * sent_value * idf
+
+        aux.append(alsent)
+
+    sentcluster_target.append(aux)
+print(len(sentcluster_source))
+print(len(sentcluster_target))
+# -------------------------------- grouping features ---------------------------------------------
+print("Linking features")
+grouped_features = {}
+for i in range(len(wclusters_source[grouped_s[i]])):
+    print(i, len(wclusters_source), len(wclusters_target[grouped_t[i]]))
+    for j in range(len(wclusters_source[grouped_s[i]])):
+        print(len())
+        if wclusters_source[grouped_s[i]][j] not in features_source:
+            index = -1
+            min = np.inf
+
+            for k in range(len(wclusters_target[grouped_t[i]])):
+                #print(i, j, k)
+                if wclusters_target[grouped_t[i]][k] not in features_source and \
+                        wclusters_target[grouped_t[i]][k] not in grouped_features:
+                    dist = abs(sentcluster_source[grouped_s[i]][j] - sentcluster_target[grouped_t[i]][k])
+
+                    if dist < min:
+                        index = k
+                        min = dist
+
+            if index > -1:
+                grouped_features[wclusters_source[grouped_s[i]][j]] = wclusters_source[grouped_s[i]][j] + "_" + \
+                                                                      wclusters_target[grouped_t[i]][index]
+                grouped_features[wclusters_target[grouped_t[i]][index]] = wclusters_source[grouped_s[i]][j] + "_" + \
+                                                                          wclusters_target[grouped_t[i]][index]
+
+print(grouped_features)
